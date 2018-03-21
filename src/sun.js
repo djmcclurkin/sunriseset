e3c644ce5490865dd0abcbba064183fa654c1dd4
David J. McClurkin <david.mcclurkin@gooddata.com>
2018-03-21 17:26:44 +0100

    Install react-day-picker

--- src/sun.js
+++ src/sun.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 export default class extends React.Component {
+
+    constructor(...args) {
+        super(...args)
+        this.state = {};
+        this.textChange = this.textChange.bind(this);
+    }
+
+    textChange(e) {
+        console.log(e.target.value);
+        this.setState({
+            text: e.target.value
+        })
+    }
+
     render() {
-        return <div>Sunrise/Sunset Application</div>;
+        return(
+            <div>
+                <h1>Sunrise/Sunset Application</h1>
+                <input onChange={this.textChange.bind(this)}/>
+                <span>{this.state.text}</span>
+            </div>
+        );
     }
 }
