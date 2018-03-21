fb51d1392add148246dfdad49e5ef3283be91f58
David J. McClurkin <david.mcclurkin@gooddata.com>
2018-03-21 17:00:27 +0100

    Create and use component Sun

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import { render } from 'react-dom';
 import React from 'react';
+import '../css/style.css';
+import Sun from './sun';
 
 const wrapper = document.getElementById('app');
 if (wrapper) {
-    render(<div>Hello world</div>, wrapper);
+    render(<Sun />, wrapper);
 }
