d91f44f65bc32391256c7df68d71259708b1c0c3
David J. McClurkin <david.mcclurkin@gooddata.com>
2018-04-26 12:11:16 +0200

    WIP: fix app

--- src/sun.js
+++ src/sun.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
+import SolarCalc from 'solar-calc';
 
 export default class extends React.Component {
 
     constructor(...args) {
-        super(...args)
-        this.state = {};
+        super(...args);
+
+        this.state = {
+            lat: 49.19,
+            lon: 16.60,
+            locName: 'Brno'
+        };
         this.handleDayClick = this.handleDayClick.bind(this);
     }
 
+    componentWillMount() {
+        this.handleDayClick(new Date());
+    }
+
     handleDayClick(date) {
+        const calc = new SolarCalc(date, this.state.lat, this.state.lon);
+
         this.setState({
-            date
+            date,
+            sunrise: calc.sunrise,
+            sunset: calc.sunset
         });
         console.log(date);
     }
@@ -21,9 +35,11 @@ export default class extends React.Component {
         return(
             <div>
                 <DayPicker
-                    selectedDays={this.state.day}
+                    selectedDays={this.state.date}
                     onDayClick={this.handleDayClick}
                 />
+                <div>{String(this.state.sunrise)}</div>
+                <div>{String(this.state.sunset)}</div>
             </div>
         );
     }
